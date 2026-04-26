ac6155fd9226f2d1256b7c0f93253f3395807327
yaksha04 <yakshatikoria04@gmail.com>
2026-04-26 22:03:00 +0530

    Implement max_subarray function for maximum sum

---
+++ DSA/maximum_subarray.py
@@ -0,0 +1,13 @@
+def max_subarray(arr):
+    max_sum = arr[0]
+    current_sum = arr[0]
+
+    for i in range(1, len(arr)):
+        current_sum = max(arr[i], current_sum + arr[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+arr = [-2,1,-3,4,-1,2,1,-5,4]
+print(max_subarray(arr))
