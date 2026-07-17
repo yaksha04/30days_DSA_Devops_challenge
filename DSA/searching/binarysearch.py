b5b7a6adf95642f8e3b6a85a88fed063f6f5278a
YAKSHA <yakshatikoria04@gmail.com>
2026-07-17 22:29:44 +0530

    Add binary search implementation in binarysearch.py
    Implement binary search algorithm in a new file.

---
+++ DSA/searching/binarysearch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        l=0
+        r=n-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                return mid
+            if target>nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        return -1            
